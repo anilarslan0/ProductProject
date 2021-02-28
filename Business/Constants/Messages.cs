334726266e800ff4fd2dfb568089801ebf5f5062
Anıl ARSLAN <anlarsln0@gmail.com>
2021-02-28 16:14:20 +0300

    new commit

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -16,5 +17,7 @@ namespace Business.Constants
         public static string ProductNameAlreadyExist = "Bu isimde başka bir ürün bulunmaktadır";
 
         public static string CategoryLimitExceded = "Kategori limiti aşıma uğradığından yeni ürün eklenemedi";
+        public static string AuthorizationDenied = "Yetkiniz yok";
+
     }
 }
--- Core/Entities/Concrete/UserOperationName.cs
+++ Core/Entities/Concrete/UserOperationName.cs
@@ -1,6 +1,6 @@
 ﻿namespace Core.Entities.Concrete
 {
-    public class UserOperationName:IEntity
+    public class UserOperationClaim:IEntity
     {
         public int Id { get; set; }
         public int UserId { get; set; }
