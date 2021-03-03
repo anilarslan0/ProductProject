dc3c5172a4d2505a6a42129911bc2a277f29cadd
Anıl ARSLAN <anlarsln0@gmail.com>
2021-03-03 22:13:05 +0300

    Authorization Operation Added

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,5 +1,7 @@
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -38,6 +40,7 @@ namespace WebAPI
             services.AddControllers();
             // services.AddSingleton<IProductService,ProductManager>(); //Arka Planda Referans Oluþtur. Ýçinde Data Tutmuyorsak Singleton kullanýlabilir.
             // services.AddSingleton<IProductDal, EfProductDal>(); //IProductDal isterse , EfProductDal ver demek
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -54,8 +57,9 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-            ServiceTool.Create(services);
+            services.AddDependencyResolvers(new ICoreModule[]{ 
+                new CoreModule()            
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
