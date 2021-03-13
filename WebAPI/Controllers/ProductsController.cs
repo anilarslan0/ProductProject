079488261ceb264c5a672fa755c28db9550ba227
Anıl ARSLAN <anlarsln0@gmail.com>
2021-03-14 02:13:42 +0300

    category api operation added

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -30,7 +30,7 @@ namespace WebAPI.Controllers
         {
             //Dependency chain
 
-            Thread.Sleep(5000);
+            Thread.Sleep(1000);
 
             var result=_productService.GetAll();
             if (result.Success)
